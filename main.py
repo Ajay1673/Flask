114f5e230ea8af1e7a62c2e2eb6b91eac0823d9a
Ajay Rathnam G <128699676+Ajay1673@users.noreply.github.com>
2023-08-06 18:10:24 +0000

    codespace is not working well, some cmd are not working

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask #import the Flask class from the flask module
+"""Python Flask: Hello World!"""
+from flask import Flask,render_template
+# import the Flask class from the flask module
 
-app = Flask(__name__) #create the application object
-
-@app.route("/") #decorator to tell Flask what URL should trigger our function hello 
-def hello(): #define the view using a function, which returns a string
-    return '<div>Welcome to Python Flask<p>My first Page</p></div>' #return a string
-    
-        
+app = Flask(__name__)
+# create the application object
 
 
+# decorator to tell Flask what URL should trigger our function hello
+@app.route("/")
+# we can write more than one decorator above a function to bind more than one URL to a function
+@app.route("/home")
+def hello():
+    """define the view using a function, which returns a string"""
+    return render_template('index.html') # return a string
+
+@app.route("/products")
+def product():
+    """products page"""
+    return render_template('products.html',item_name='Try our new product')
